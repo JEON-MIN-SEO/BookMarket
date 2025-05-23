b3a32dc8eda7612699f8e6ec451dba713056e680
jeonminseo <jmsparrowk@naver.com>
2025-05-23 18:30:52 +0900

    Member Repository

--- src/main/java/mate/bookmarket/domain/member/repository/MemberRepository.java
+++ src/main/java/mate/bookmarket/domain/member/repository/MemberRepository.java
@@ -1,4 +1,13 @@
 package mate.bookmarket.domain.member.repository;
 
-public interface MemberRepository {
+import mate.bookmarket.domain.member.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUserId(String userId);
+
+    MemberEntity findByUserId(String userId);
 }
