3a21356a9c7a3193866df5bcfd371903d6e0cb80
jeonminseo <jmsparrowk@naver.com>
2025-05-23 18:31:13 +0900

    MemberEntity

--- src/main/java/mate/bookmarket/domain/member/entity/MemberEntity.java
+++ src/main/java/mate/bookmarket/domain/member/entity/MemberEntity.java
@@ -1,15 +1,19 @@
 package mate.bookmarket.domain.member.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 import mate.bookmarket.common.enums.MemberGrade;
+import mate.bookmarket.domain.member.dto.request.MemberSignUpDTO;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "Member")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberEntity {
 
     @Id
@@ -29,14 +33,26 @@ public class MemberEntity {
     private String name;
 
     @Column
-    private BigDecimal point;
+    private BigDecimal point = new BigDecimal("0");
 
     @Column
-    private MemberGrade myGrade;
+    @Enumerated(EnumType.STRING) //배열로 나오는게 아니라 String으로 출력됨
+    private MemberGrade myGrade = MemberGrade.BRONZE;
 
     @Column
-    private Boolean isSuspended;
+    private Boolean isSuspended = false;
 
     @Column
-    private Boolean isWithdrawn;
+    private Boolean isWithdrawn = false;
+
+    public void signUpEntity(MemberSignUpDTO memberSignUpDTO) {
+        this.userId = memberSignUpDTO.getUserId();
+        this.password = memberSignUpDTO.getPassword();
+        this.email = memberSignUpDTO.getEmail();
+        this.name = memberSignUpDTO.getName();
+//        this.myGrade = MemberGrade.BRONZE;
+//        this.point = new BigDecimal("0");
+//        this.isSuspended = false;
+//        this.isWithdrawn = false;
+    }
 }
