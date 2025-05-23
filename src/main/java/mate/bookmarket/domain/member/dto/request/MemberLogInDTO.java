a25ebbb6ad3892811f1c2482c447120887a485c1
jeonminseo <jmsparrowk@naver.com>
2025-05-23 18:31:02 +0900

    Login DTO

--- src/main/java/mate/bookmarket/domain/member/dto/request/MemberLogInDTO.java
+++ src/main/java/mate/bookmarket/domain/member/dto/request/MemberLogInDTO.java
@@ -1,4 +1,15 @@
 package mate.bookmarket.domain.member.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
 public class MemberLogInDTO {
+
+    @NotBlank
+    private String userId;
+
+    @NotBlank
+    private String password;
+
 }
