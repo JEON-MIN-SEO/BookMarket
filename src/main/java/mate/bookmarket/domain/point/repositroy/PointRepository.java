d525e852d5070078e6b609aa57a2cdb0fb4e0214
jeonminseo <jmsparrowk@naver.com>
2025-05-25 01:09:19 +0900

    포인트 레포지터리

---
+++ src/main/java/mate/bookmarket/domain/point/repositroy/PointRepository.java
@@ -0,0 +1,7 @@
+package mate.bookmarket.domain.point.repositroy;
+
+import mate.bookmarket.domain.point.entity.PointEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointRepository extends JpaRepository<PointEntity, Long> {
+}
