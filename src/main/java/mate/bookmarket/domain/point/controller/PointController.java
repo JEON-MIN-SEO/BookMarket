452d8fadb642e9fac64ce2c819265a25108ba418
jeonminseo <jmsparrowk@naver.com>
2025-05-25 01:10:36 +0900

    포인트 테스트API 작성

--- src/main/java/mate/bookmarket/domain/point/controller/PointController.java
+++ src/main/java/mate/bookmarket/domain/point/controller/PointController.java
@@ -1,10 +1,26 @@
 package mate.bookmarket.domain.point.controller;
 
+import lombok.RequiredArgsConstructor;
+import mate.bookmarket.domain.point.dto.request.PointPendingRequestDTO;
+import mate.bookmarket.domain.point.service.PointService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/point")
 public class PointController {
 
-    //포인트 등록
+    private final PointService pointService;
 
-    //포인트
+    //테스트 - 포인트 팬딩 컨트롤러
+    @PostMapping("/pending")
+    public BigDecimal pendingPoint(@RequestBody PointPendingRequestDTO requestDTO) {
+        return pointService.pendingPoint(requestDTO);
+    }
 
-    //
 }
