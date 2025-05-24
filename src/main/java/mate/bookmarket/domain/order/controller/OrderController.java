739baa4fda674888bb54be8afb83494d39b8049f
jeonminseo <jmsparrowk@naver.com>
2025-05-25 01:10:43 +0900

    작업 중

---
+++ src/main/java/mate/bookmarket/domain/order/controller/OrderController.java
@@ -0,0 +1,10 @@
+package mate.bookmarket.domain.order.controller;
+
+public class OrderController {
+
+    //도서 구매 -> 포인트 팬딩
+
+    //도서 구매 취소 -> 포인트 팬딩 취소
+
+    //
+}
---
+++ src/main/java/mate/bookmarket/domain/order/service/OrderService.java
@@ -0,0 +1,17 @@
+package mate.bookmarket.domain.order.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    //주문 생성
+
+    //주문 취소
+
+    //환불
+
+    //주문 상세조회
+
+    //주문 목록 조회
+}
