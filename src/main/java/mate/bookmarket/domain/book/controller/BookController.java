4bac5c6466af1a25f4966391efe7e6b5ddc96ff3
jeonminseo <jmsparrowk@naver.com>
2025-05-25 01:10:54 +0900

    작업 중

---
+++ src/main/java/mate/bookmarket/domain/book/controller/BookController.java
@@ -0,0 +1,18 @@
+package mate.bookmarket.domain.book.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book") //알아보기
+@RequiredArgsConstructor //알아보기
+public class BookController {
+
+
+    //도서 검색
+
+    //도서 상세정보
+
+    //도서 등록
+}
