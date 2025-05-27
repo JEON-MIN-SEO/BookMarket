981b6f074df154379cc6c3d409e9e2968889f7d5
jeonminseo <jmsparrowk@naver.com>
2025-05-27 14:52:34 +0900

    멤버 아이디 반환 DTO 추가

---
+++ src/main/java/mate/bookmarket/domain/member/dto/response/MemberReturnIdDTO.java
@@ -0,0 +1,14 @@
+package mate.bookmarket.domain.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class MemberReturnIdDTO {
+
+    private String userId;
+
+}
