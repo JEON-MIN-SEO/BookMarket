2be2e07a8624824ba2a98c6a3756a2d2e415e5b9
jeonminseo <jmsparrowk@naver.com>
2025-05-27 14:52:21 +0900

    커스텀 멤버 ID 찾는 쿼리 추가

--- src/main/java/mate/bookmarket/domain/member/repository/MemberRepository.java
+++ src/main/java/mate/bookmarket/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package mate.bookmarket.domain.member.repository;
 
 import mate.bookmarket.domain.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
@@ -11,5 +13,11 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
     MemberEntity findByUserId(String userId);
 
+    //findById랑 같은거임 => Entity의 @Id를 기준으로 찾는거임
     MemberEntity findByMemberId(Long memberId);
+
+    @Query("SELECT m FROM MemberEntity m WHERE m.memberId = :memberId")
+    MemberEntity customMemberId(@Param("memberId")Long memberId);
+
+    MemberEntity findByEmail(String email);
 }
